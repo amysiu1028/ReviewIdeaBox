d6ccd90c7d20b8e23290c70c7ee5c080db8cd144
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-12 15:30:17 -0400

    Add Query Selectors for the input field

---
+++ main.js
@@ -0,0 +1,6 @@
+//Query Selectors
+var titleInput = document.querySelector("#title-input")
+var bodyInput = document.querySelector("#body-input")
+var saveButton = document.querySelector(".save-button")
+
+//Event Listeners
