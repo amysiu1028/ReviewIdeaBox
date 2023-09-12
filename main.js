d5768830670873d7db60b23caec51631903e9c57
Arden Ranta <arranta20@gmail.com>
2023-09-12 16:27:14 -0700

    feature/Add new cards and beginning of save button

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ var bodyInput = document.querySelector("#body-input");
 var saveButton = document.querySelector(".save-button");
 var ideaHeader = document.querySelector("h2");
 var ideaBody = document.querySelector("p");
+var parentElement = document.getElementById("parent-element");
+
 //Event Listeners
 saveButton.addEventListener("click",function(event){
-    event.preventDefault(),
-    saveIdea(), 
-    displayIdeas()});
+  event.preventDefault(),
+  saveIdea(),
+  displayIdeas(),
+  titleInput.value = "",
+  bodyInput.value = ""
+  });
+  window.addEventListener("load", disableSaveButton);
 
 var ideasArray = [];
 var newestIdea = {}
@@ -26,19 +32,30 @@ function saveIdea(){
 //create a new function that iterates through the ideasArray and createElement to display on the page
     //if statement that prevents more than 3 displaying at once.
     //could also use a slice method to only display the first three.
+// function displayIdeas() {
+//     ideaHeader.innerHTML = titleInput.value;
+//     ideaBody.innerHTML = bodyInput.value;
+// }
+
 function displayIdeas() {
-    ideaHeader.innerHTML = titleInput.value;
-    ideaBody.innerHTML = bodyInput.value;
-    // for (var i = 0; i < ideasArray.length; i++) {
-    //   if
-    // }
+  var ideaCardsParentElement = document.getElementById("idea-cards");
+  var newIdeaArticle = document.createElement('article');
+  newIdeaArticle.classList.add('article');
+  ideaCardsParentElement.appendChild(newIdeaArticle);
+  var newIdeaHeading = document.createElement('h2');
+  var newIdeaParagraph = document.createElement('p');
+  newIdeaHeading.textContent = newestIdea.title;
+  newIdeaParagraph.textContent = newestIdea.body;
+  newIdeaArticle.appendChild(newIdeaHeading);
+  newIdeaArticle.appendChild(newIdeaParagraph);
 }
 
-
-
-
-
-
-
-
+function disableSaveButton(){
+// write in saveButton.disabled = false later to enable the saveButton again
+  if (titleInput.value = "" ||bodyInput.value = "") {
+    saveButton.disabled = true;
+    saveButton.style.backgroundColor = "#EAEAF4";
+  // add or remove classlist to change cursor icon
+  }
+}
 
