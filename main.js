d22b6dcaa818424b787eda21760eef4f39fad4cf
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-13 17:24:10 -0400

    Add DRYer displayIdeas function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var titleInput = document.querySelector("#title-input");
 var bodyInput = document.querySelector("#body-input");
 var saveButton = document.querySelector(".save-button");
-
+var ideaCardSection = document.querySelector(".idea-cards")
 //Event Listeners
 saveButton.addEventListener("click", function(event){
   event.preventDefault(),
@@ -30,30 +30,49 @@ function saveIdea(){
     //if statement that prevents more than 3 displaying at once.
     //could also use a slice method to only display the first three.
 
-function displayIdeas() {
-    var ideaCardsParentElement = document.getElementById("idea-cards");
-    var newIdeaArticle = document.createElement('article');
-    newIdeaArticle.classList.add('article');
-    ideaCardsParentElement.appendChild(newIdeaArticle);
-    var newButtonWrapper = document.createElement('div');
-    newButtonWrapper.classList.add('button-wrapper');
-    var newTextContainer = document.createElement('div');
-    newTextContainer.classList.add('text-container');
-    newIdeaArticle.appendChild(newTextContainer);
-    newIdeaArticle.appendChild(newButtonWrapper);
-    var newFavButton = document.createElement('button');
-    // newFavButton.classList.add('favorite-button');
-    // newDelButton.classList.add('delete-button');
-    var newDelButton = document.createElement('button');
-    var newIdeaHeading = document.createElement('h2');
-    var newIdeaParagraph = document.createElement('p');
-    newIdeaHeading.textContent = newestIdea.title;
-    newIdeaParagraph.textContent = newestIdea.body;
-    newButtonWrapper.appendChild(newDelButton);
-    newButtonWrapper.appendChild(newFavButton);
-    newTextContainer.appendChild(newIdeaHeading);
-    newTextContainer.appendChild(newIdeaParagraph);
-  }
+// function displayIdeas() {
+//     var ideaCardsParentElement = document.getElementById("idea-cards");
+//     var newIdeaArticle = document.createElement('article');
+//     newIdeaArticle.classList.add('article');
+//     ideaCardsParentElement.appendChild(newIdeaArticle);
+//     var newButtonWrapper = document.createElement('div');
+//     newButtonWrapper.classList.add('button-wrapper');
+//     var newTextContainer = document.createElement('div');
+//     newTextContainer.classList.add('text-container');
+//     newIdeaArticle.appendChild(newTextContainer);
+//     newIdeaArticle.appendChild(newButtonWrapper);
+//     var newFavButton = document.createElement('button');
+//     // newFavButton.classList.add('favorite-button');
+//     // newDelButton.classList.add('delete-button');
+//     var newDelButton = document.createElement('button');
+//     var newIdeaHeading = document.createElement('h2');
+//     var newIdeaParagraph = document.createElement('p');
+//     newIdeaHeading.textContent = newestIdea.title;
+//     newIdeaParagraph.textContent = newestIdea.body;
+//     newButtonWrapper.appendChild(newDelButton);
+//     newButtonWrapper.appendChild(newFavButton);
+//     newTextContainer.appendChild(newIdeaHeading);
+//     newTextContainer.appendChild(newIdeaParagraph);
+//   }
+
+  function displayIdeas() {
+    ideaCardSection.innerHTML = "";
+    for (var i = 0; i < ideasArray.length; i++){
+    ideaCardSection.innerHTML +=
+    `<article class="new-idea-card">
+        <div class="button-wrapper">
+            <button class="favorite-button">(img)</button>
+            <button class="delete-button">(img)</button>
+        </div>
+        <section class="text-container">
+            <h2 class="card-title">${ideasArray[i].title}</h2>
+            <p class="card-body">${ideasArray[i].body}</p>
+        </section>
+    </article>
+        `
+        console.log(`${ideasArray[i].title}`)
+}
+}
 
 // function disableSaveButton(){
 // // write in saveButton.disabled = false later to enable the saveButton again
