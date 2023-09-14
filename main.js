a27afb845a22fa741a203cfe3a660fbb33024bba
Emalee Poellot <emalee.poellot@gmail.com>
2023-09-14 11:48:59 -1000

    feat/Create a favorited function which makes the star turn orange when the user clicks the button

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ var titleInput = document.querySelector("#title-input");
 var bodyInput = document.querySelector("#body-input");
 var saveButton = document.querySelector(".save-button");
 var ideaCardSection = document.querySelector(".idea-cards")
+var favoriteButton = document.querySelectorAll(".favorite-button");
+var deleteButton = document.querySelectorAll(".delete-button");
+var buttonWrapper = document.querySelector(".button-wrapper");
+// var activeStarImage = document.querySelector(".favorite-button-active")
 
 //Event Listeners
-
-titleInput.addEventListener("input", saveButtonToggle)
-bodyInput.addEventListener("input", saveButtonToggle)
-saveButton.addEventListener("mouseover", saveButtonToggle)
+ideaCardSection.addEventListener("click", favorited);
+titleInput.addEventListener("input", saveButtonToggle);
+bodyInput.addEventListener("input", saveButtonToggle);
+saveButton.addEventListener("mouseover", saveButtonToggle);
 saveButton.addEventListener("click", function(event){
   event.preventDefault(),
   saveIdea(),
@@ -22,8 +26,10 @@ saveButton.addEventListener("click", function(event){
 // Event Handlers
 
 var ideasArray = [];
-var newestIdea = {}
-function saveIdea(){
+var newestIdea = {};
+var coloredFavoriteButton = `<button class="favorite-button"><img class="favorite-button-active" src=assets/star-active.svg></button>`; 
+
+function saveIdea() {
     newestIdea = {
         title: titleInput.value,
         body: bodyInput.value,
@@ -53,14 +59,27 @@ function displayIdeas() {
     </article>
     `
   }
-}
+};
 
-function saveButtonToggle(){
+
+function saveButtonToggle() {
   if (titleInput.value !== "" && bodyInput.value !== "") {
-    saveButton.classList.remove("disabled")
+    saveButton.classList.remove("disabled");
     saveButton.disabled = false;
   } else {
     saveButton.disabled = true;
     saveButton.classList.add("disabled");
   }
-}
+};
+
+function favorited(event) {
+  if (event.target.classList.contains("favorite-button")) {
+    var userFavoritedButton = event.target.querySelector(".favorite-button-active");
+    if (userFavoritedButton.src.endsWith("star.svg")) {
+      userFavoritedButton.src = "assets/star-active.svg";
+    } else if (userFavoritedButton.src.endsWith("star-active.svg")) {
+      userFavoritedButton.src = "assets/star.svg";
+    }
+  }
+};
+
