02ab3a261f68cb2efc9aab2e67fec08b23eeee58
Arden Ranta <arranta20@gmail.com>
2023-09-13 17:23:05 -0700

    bugfix/Save button now follows rubric

--- main.js
+++ main.js
@@ -2,17 +2,21 @@
 var titleInput = document.querySelector("#title-input");
 var bodyInput = document.querySelector("#body-input");
 var saveButton = document.querySelector(".save-button");
+var ideaCardSection = document.querySelector(".idea-cards")
 
 //Event Listeners
+titleInput.addEventListener("input", enableSaveButton)
+bodyInput.addEventListener("input", enableSaveButton)
+saveButton.addEventListener("mouseover", disableSaveButton, enableSaveButton)
 saveButton.addEventListener("click", function(event){
-  event.preventDefault(),
+    event.preventDefault(),
   saveIdea(),
   displayIdeas(),
   titleInput.value = "",
   bodyInput.value = ""
   });
-// window.addEventListener("load", disableSaveButton);
 
+// Event Handlers
 var ideasArray = [];
 var newestIdea = {}
 
@@ -31,36 +35,34 @@ function saveIdea(){
     //could also use a slice method to only display the first three.
 
 function displayIdeas() {
-    var ideaCardsParentElement = document.getElementById("idea-cards");
-    var newIdeaArticle = document.createElement('article');
-    newIdeaArticle.classList.add('article');
-    ideaCardsParentElement.appendChild(newIdeaArticle);
-    var newButtonWrapper = document.createElement('div');
-    newButtonWrapper.classList.add('button-wrapper');
-    var newTextContainer = document.createElement('div');
-    newTextContainer.classList.add('text-container');
-    newIdeaArticle.appendChild(newTextContainer);
-    newIdeaArticle.appendChild(newButtonWrapper);
-    var newFavButton = document.createElement('button');
-    // newFavButton.classList.add('favorite-button');
-    // newDelButton.classList.add('delete-button');
-    var newDelButton = document.createElement('button');
-    var newIdeaHeading = document.createElement('h2');
-    var newIdeaParagraph = document.createElement('p');
-    newIdeaHeading.textContent = newestIdea.title;
-    newIdeaParagraph.textContent = newestIdea.body;
-    newButtonWrapper.appendChild(newDelButton);
-    newButtonWrapper.appendChild(newFavButton);
-    newTextContainer.appendChild(newIdeaHeading);
-    newTextContainer.appendChild(newIdeaParagraph);
+  ideaCardSection.innerHTML = "";
+  for (var i = 0; i < ideasArray.length; i++){
+    ideaCardSection.innerHTML += 
+    `<article class="new-idea-card" id=${ideasArray[i].title}>
+      <div class="button-wrapper">
+          <button class="favorite-button">(img)</button>
+          <button class="delete-button">(img)</button>
+      </div>
+      <section class="text-container">
+          <h2 class="card-title">${ideasArray[i].title}</h2>
+          <p class="card-body">${ideasArray[i].body}</p>
+      </section>
+    </article>
+    `
   }
+}
 
-// function disableSaveButton(){
-// // write in saveButton.disabled = false later to enable the saveButton again
-//   if (titleInput.value = "" ||bodyInput.value = "") {
-//     saveButton.disabled = true;
-//     saveButton.style.backgroundColor = "#EAEAF4";
-//   // add or remove classlist to change cursor icon
-//   }
-// }
+function enableSaveButton(){
+  if (titleInput.value !== "" && bodyInput.value !== "") {
+     saveButton.classList.remove("disabled") 
+      saveButton.disabled = false;
 
+     
+  }
+}
+
+function disableSaveButton(){
+  if (titleInput.value.length === 0 && bodyInput.value.length === 0){
+      saveButton.classList.add("disabled")
+  }
+}
