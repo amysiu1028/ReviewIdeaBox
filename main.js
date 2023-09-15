bc00d03bf728c670ede4976be4457bf21e438dec
Emalee Poellot <emalee.poellot@gmail.com>
2023-09-15 05:40:31 -1000

    feat/Create a function to change views to favorited ideas, non functional

--- main.js
+++ main.js
@@ -83,3 +83,55 @@ function favorited(event) {
   }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var showAllIdeasButton = document.querySelector(".show-all-ideas");
+var showStarredIdeasButton = document.querySelector(".show-starred-ideas");
+var favoriteIdeasSection = document.querySelector(".favorite-ideas-section");
+
+
+showAllIdeasButton.addEventListener("click", displayFavoriteIdeas) 
+
+function displayFavoriteIdeas() {
+  showStarredIdeasButton.classList.add("hidden");
+  showAllIdeasButton.classList.remove("hidden");
+  favoriteIdeasSection.classList.remove("hidden");
+  ideaCardSection.classList.add("hidden");
+  favoriteIdeasSection.innerHTML = "";
+  for (var i = 0; i < showStarredIdeasButton.length; i++){
+    favoriteIdeasSection.innerHTML +=   
+    `<article class="favorite-ideas-card" id=${ideasArray[i].title}>
+    <div class="button-wrapper">
+        <button class="favorite-button"><img class="favorite-button-active" src=assets/star.svg></button>
+        <button class="delete-button"><img class="delete-button-active" src=assets/delete.svg></button>
+    </div>
+    <section class="text-container">
+        <h2 class="card-title">${ideasArray[i].title}</h2>
+        <p class="card-body">${ideasArray[i].body}</p>
+    </section>
+  </article>
+  `
+  }
+}
