28b22f1c9c636073b2160234f19d8937e036edd1
Arden Ranta <arranta20@gmail.com>
2023-09-15 22:46:43 -0700

    feat/Allow a click on the correct card for starring the function

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ var favoriteIdeasSection = document.querySelector(".favorite-ideas-section");
 var favoriteButtonOrange = document.querySelector(".favorite-button-or");
 
 //Event Listeners
-ideaCardSection.addEventListener("click", favorited);
+ideaCardSection.addEventListener("click", function(event) {
+  currentClick = event.target.closest(".new-idea-card");
+  if (currentClick) {
+    favorited(currentClick)
+  }
+});
+
 titleInput.addEventListener("input", saveButtonToggle);
 bodyInput.addEventListener("input", saveButtonToggle);
 saveButton.addEventListener("mouseover", saveButtonToggle);
@@ -25,18 +31,17 @@ saveButton.addEventListener("click", function(event){
   bodyInput.value = "",
   saveButtonToggle()
   });
-
 // ideaCardSection.addEventListener("click", function(event) {
 //     deleteCard(event);
   // });
-showStarredIdeasButton.addEventListener("click", displayFavoriteIdeas); 
+showStarredIdeasButton.addEventListener("click", displayFavoriteIdeas);
 showAllIdeasButton.addEventListener("click", showAllIdeas);
 
 // Event Handlers
-
 var ideasArray = [];
 var newestIdea = {};
 var starredIdeas = [];
+var currentClick;
 // var userFavoritedButton;
 
 function saveIdea() {
@@ -54,10 +59,10 @@ function displayIdeas() {
   ideaCardSection.innerHTML = "";
   for (var i = 0; i < ideasArray.length; i++){
     ideaCardSection.innerHTML +=
-    `<article class="new-idea-card">
+    `<article class="new-idea-card" id="${ideasArray[i].body}">
       <div class="button-wrapper">
-          <button class="favorite-button" id=${ideasArray[i].body}></button>
-          <button class="favorite-button-or hidden" id="${ideasArray[i].body}"></button>
+          <button class="favorite-button" id="${ideasArray[i].title} + white"></button>
+          <button class="favorite-button-or hidden" id="${ideasArray[i].title} + orange"></button>
           <button class="delete-button" id="${ideasArray[i].title}" ><img class="delete-button-active" src=assets/delete.svg></button>
       </div>
       <section class="text-container">
@@ -84,49 +89,48 @@ function saveButtonToggle() {
 //     if (ideasArray[i].title === event.target.id) {
 //       ideasArray.splice(i, 1);
 //     }
-//   } 
+//   }
 //   displayIdeas();
 // }
 
-function favorited(event) {
-  // for (var i = 0; i < ideasArray.length; i ++){
-  //   if (event.target.id === ideasArray[i].body){
-        var userFavoritedButton = document.querySelector(".favorite-button");
-        var userFavoritedButtonOr = document.querySelector(".favorite-button-or");
-        if (event.target.classList.contains("favorite-button")) {
-          userFavoritedButton.classList.add("hidden");
-          userFavoritedButtonOr.classList.remove("hidden");
-        } else {
-          userFavoritedButton.classList.remove("hidden");
-          userFavoritedButtonOr.classList.add("hidden");
+function favorited() {
+  console.log(currentClick.id, "<this is current.id")
+        var userFavoritedButton = currentClick.querySelector(".favorite-button");
+        var userFavoritedButtonOr = currentClick.querySelector(".favorite-button-or");
+        console.log(userFavoritedButton, "favoritebutton")
+        console.log(userFavoritedButtonOr, "orangefav")
+        if (currentClick.classList.contains("new-idea-card")) {
+          userFavoritedButton.classList.toggle("hidden");
+          userFavoritedButtonOr.classList.toggle("hidden");
+          isOrange();
+        // } else {
+        //   userFavoritedButton.classList.remove("hidden");
+        //   userFavoritedButtonOr.classList.add("hidden");
         }
-        isOrange(event);
-        }
-  //   }
-  // }
-
-
-
-function isOrange(event) {
+}
+       
+function isOrange() {
+  console.log(currentClick.id)
   for (var i = 0; i < ideasArray.length; i++) {
-    if (ideasArray[i].body === event.target.id && ideasArray[i].isOrange === false) {
+    if (ideasArray[i].body === currentClick.id) { 
+      console.log(currentClick.id, "<this is current.id");
+      if (ideasArray[i].isOrange === false) {
       ideasArray[i].isOrange = true;
       starredIdeas.push(ideasArray[i]);
       console.log(ideasArray[i].isOrange, starredIdeas, "I for loop");
+      }
     } else {
+      console.log(ideasArray[i].isOrange, starredIdeas, "j for loop");
       ideasArray[i].isOrange = false;
       for (var j = 0; j < starredIdeas.length; j++) {
-        if (starredIdeas[j] === ideasArray[i]) {
+        if (starredIdeas[j].id === ideasArray[i].id) {
           starredIdeas.splice(j, 1);
-          console.log(ideasArray[i].isOrange, starredIdeas, "j for loop");
         }
       }
     }
   }
-  event.preventDefault();
   return starredIdeas;
 }
-
 
 function displayFavoriteIdeas() {
   showStarredIdeasButton.classList.add("hidden");
@@ -135,11 +139,11 @@ function displayFavoriteIdeas() {
   ideaCardSection.classList.add("hidden");
   favoriteIdeasSection.innerHTML = "";
   for (var i = 0; i < showStarredIdeasButton.length; i++){
-    favoriteIdeasSection.innerHTML +=   
-    `<article class="favorite-ideas-card" id=${starredIdeas[i].title}>
+    favoriteIdeasSection.innerHTML +=
+    `<article class="favorite-ideas-card" id=${starredIdeas[i].body}>
     <div class="button-wrapper">
-        <button class="favorite-button"><img class="favorite-button-active" src=assets/star.svg></button>
-        <button class="delete-button"><img class="delete-button-active" src=assets/delete.svg></button>
+        <button class="favorite-button"></button>
+        <button class="delete-button"></button>
     </div>
     <section class="text-container">
         <h2 class="card-title">${starredIdeas[i].title}</h2>
@@ -149,15 +153,12 @@ function displayFavoriteIdeas() {
   `
   }
 }
-
 function showAllIdeas() {
   showStarredIdeasButton.classList.remove("hidden");
   showAllIdeasButton.classList.add("hidden");
   favoriteIdeasSection.classList.add("hidden");
   ideaCardSection.classList.remove("hidden");
 }
-
-
 // function favorited(event) {
 //   var userFavoritedButton = document.querySelector(".favorite-button");
 //   var userFavoritedButtonOr = document.querySelector(".favorite-button-or");
@@ -170,9 +171,6 @@ function showAllIdeas() {
 //   }
 //   isOrange(event);
 //   }
-
-
-
 // function isOrange(event) {
 //   for (var i = 0; i < ideasArray.length; i++) {
 //     if (ideasArray[i].body === event.target.id && ideasArray[i].isOrange === false) {
